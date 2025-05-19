45be1a5dd9424b393c3975f182179cce717bd4a3
wangwenxiang <1570679451@qq.com>
2025-05-19 13:35:00 +0800

    rand passwrod 添加注释

--- src/base/parser.rs
+++ src/base/parser.rs
@@ -1,14 +1,20 @@
 use crate::csv::OutputFormat;
 use std::path::Path;
 
+// 判断文件是否存在
 pub fn input_file_exists(filename: &str) -> Result<String, String> {
+    // 创建一个Path对象，传入文件名
     if Path::new(filename).exists() {
+        // 如果文件存在，返回文件名
         Ok(filename.to_string())
     } else {
+        // 如果文件不存在，返回错误信息
         Err(format!("File {} does not exist", filename))
     }
 }
 
+// 解析格式字符串，返回OutputFormat或错误
 pub fn parser_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    // 将格式字符串解析为OutputFormat
     format.parse()
 }
--- src/genpass/process.rs
+++ src/genpass/process.rs
@@ -4,16 +4,27 @@ const UPPERCASE: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const LOWERCASE: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
 const NUMBERS: &[u8] = b"0123456789";
 const SYMBOLS: &[u8] = b"!@#$%^&*_+-=|?";
+
+// 生成密码的函数
 pub fn genpass_process(
+    // 密码长度
     length: u8,
+    // 是否包含大写字母
     uppercase: bool,
+    // 是否包含小写字母
     lowercase: bool,
+    // 是否包含数字
     numbers: bool,
+    // 是否包含符号
     symbols: bool,
 ) -> anyhow::Result<()> {
+    // 创建随机数生成器
     let mut rng = rand::rng();
+    // 创建密码的向量
     let mut password: Vec<u8> = Vec::new();
+    // 创建字符的向量
     let mut chars = Vec::new();
+    // 如果包含大写字母，则将大写字母添加到字符向量中，并从大写字母中随机选择一个字符添加到密码向量中
     if uppercase {
         chars.extend_from_slice(UPPERCASE);
         password.push(
@@ -22,6 +33,7 @@ pub fn genpass_process(
                 .expect("UPPERCASE won't be empty"),
         );
     }
+    // 如果包含小写字母，则将小写字母添加到字符向量中，并从小写字母中随机选择一个字符添加到密码向量中
     if lowercase {
         chars.extend_from_slice(LOWERCASE);
         password.push(
@@ -30,25 +42,34 @@ pub fn genpass_process(
                 .expect("LOWERCASE won't be empty"),
         );
     }
+    // 如果包含数字，则将数字添加到字符向量中，并从数字中随机选择一个字符添加到密码向量中
     if numbers {
         chars.extend_from_slice(NUMBERS);
         password.push(*NUMBERS.choose(&mut rng).expect("NUMBERS won't be empty"));
     }
+    // 如果包含符号，则将符号添加到字符向量中，并从符号中随机选择一个字符添加到密码向量中
     if symbols {
         chars.extend_from_slice(SYMBOLS);
         password.push(*SYMBOLS.choose(&mut rng).expect("SYMBOLS won't be empty"));
     }
 
+    // 循环添加剩余的字符到密码向量中
     for _ in 0..(length - password.len() as u8) {
         let c = chars.choose(&mut rng).expect("chars won't be empty");
         password.push(*c);
     }
+    // 随机打乱密码向量中的字符
     password.shuffle(&mut rng);
 
+    // 将密码向量转换为字符串
     let password = String::from_utf8(password)?;
+    // 打印密码
     println!("password: {}", password);
+    // 使用zxcvbn库计算密码的强度
     let result = zxcvbn::zxcvbn(&password, &[]);
+    // 打印密码强度
     println!("Password level: {}", result.score());
+    // 返回结果
 
     Ok(())
 }
